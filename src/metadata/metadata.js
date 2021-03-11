5cb6428ac4d48c63adf34158f9a98048b6577325
cagi2018 <dmitry_penzar_1996@mail.ru>
2021-03-11 11:58:38 +0300

    update papers

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -113,42 +113,59 @@ I've attend a huge variety of additional courses, including:
 		{
 			header: "Insights gained from a comprehensive all-against-all transcription factor binding motif benchmarking study",
 		theme: ["epigenomics", "visualization", "regulatory regions"],
-		contents: `
+		contents: `Ambrosini Giovanna, Vorontsov Ilya, <strong>Penzar Dmitry</strong>, Groux Romain, 
+		Fornes Oriol, Nikolaeva Daria D., Ballester Benoit, Grau Jan,
+		Grosse Ivo, Makeev Vsevolod, Kulakovskiy Ivan, Bucher Philipp
 		`},
 		{
 		header: "The species Severe acute respiratory syndrome-related coronavirus: classifying 2019-nCoV and naming it SARS-CoV-2",
 		theme: ["virology", "R", "software development"],
-		contents: `
+		contents: `Gorbalenya Alexander E., Baker Susan C., Baric Ralph S., de Groot Raoul J.,
+		Drosten Christian, Gulyaeva Anastasia A., Haagmans Bart L., Lauber Chris, Leontovich Andrey M., 
+		Neuman Benjamin W., <strong>Penzar Dmitry</strong>, Perlman Stanley, Poon Leo L.M., Samborskiy Dmitry V.,
+		Sidorov Igor A., Sola Isabel, Ziebuhr John
 		`},
 		{
 		header: "H3K4me3, H3K9ac, H3K27ac, H3K27me3 and H3K9me3 Histone Tags Suggest Distinct Regulatory Evolution of Open and Condensed Chromatin Landmarks",
 		theme: ["epigenomics"],
-		contents: `
+		contents: `Igolkina Anna A., Zinkevich Arsenii, Karandasheva Kristina O., Popov Aleksey A.,
+		Selifanova Maria V., Nikolaeva Daria, Tkachev Victor, 
+		<strong>Penzar Dmitry</strong>, Nikitin Daniil M., Buzdin Anton
 		`},
 		{
 			header: "Integration of Multiple Epigenomic Marks Improves Prediction of Variant Impact in Saturation Mutagenesis Reporter Assayheader",
 		theme: ["epigenomics", "machine learning", "regulatory regions"],
 		contents: `
-		`},
+		Shigaki Dustin, Adato Orit, Adhikar Aashish N., Dong Shengcheng, Hawkins-Hooker Alex,
+		Inoue Fumitaka, Juven-Gershon Tamar, Kenlay Henry, Martin Beth, Patra Ayoti, <strong>Penzar Dmitry P.</strong>, 
+		Schubach Max, Xiong Chenling, Yan Zhongxia, Boyle Alan P., Kreimer Anat, Kulakovskiy Ivan V., 
+		Reid John, Unger Ron, Yosef Nir, Shendure Jay, Ahituv Nadav, Kircher Martin, Beer Michael A.`},
 		{
 			header: "Retroelement—Linked Transcription Factor Binding Patterns Point to Quickly Developing Molecular Pathways in Human Evolution",
 		theme: ["epigenomics", "visualization", "regulatory regions"],
 		contents: `
+		Nikitin Daniil, Garazha Andrew, Sorokin Maxim, <strong>Penzar Dmitry</strong>, Tkachev Victor, 
+		Markov Alexander, Gaifullin Nurshat, Borger Pieter, Poltorak Alexander, Buzdin Anton
 		`},
 		{
 			header: "What Do Neighbors Tell About You: The Local Context of Cis-Regulatory Modules Complicates Prediction of Regulatory Variants",
 		theme: ["machine learning", "data leakage", "regulatory regions"],
 		contents: `
+		<strong>Penzar Dmitry D.</strong>, Zinkevich Arsenii O., Vorontsov Ilya E., Sitnik Vasily V., 
+		Favorov Alexander V., Makeev Vsevolod J., Kulakovskiy Ivan V.
 		`},
 		{
 			header: "PQ, a new program for phylogeny reconstruction",
 		theme: ["phylogeny"],
 		contents: `
+		<strong>Dmitry Penzar</strong>, Mikhail Krivozubov, Sergey Spirin
 		`},
 		{
 			header: "Profiling of Human Molecular Pathways Affected by Retrotransposons at the Level of Regulation by Transcription Factor Proteins",
 		theme: ["epigenomics", "visualization", "regulatory regions"],
 		contents: `
+		Nikitin Daniil, <strong>Penzar Dmitry</strong>, Garazha Andrew, Sorokin Maxim, Tkachev Victor, 
+		Borisov Nicolas, Poltorak Alexander, Prassolov Vladimir, Buzdin Anton A.
 		`},
  ]
 };
